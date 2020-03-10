0d75ab99079a3211b7fc74aeac6d136f2b511f71
mazunki <rolferen@gmail.com>
2020-03-10 01:16:52 +0100

    first commit

---
+++ src/tech/mazunki/Screwdriver.java
@@ -0,0 +1,18 @@
+package tech.mazunki;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Screwdriver extends JavaPlugin {// implements Listeners {
+	@Override
+	public void onEnable() {
+		// run on server initialization
+	}
+
+	@Override
+	public void onDisable() {
+
+	}
+
+	public static void main(String[] args) {
+		System.out.println("I work.");
+	}
+}
